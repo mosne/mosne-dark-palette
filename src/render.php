1bb3f140def013df1d9c8a319b1155dd04184728
Paolo Tesei <ptesei@beapi.fr>
2025-01-30 09:45:36 +0100

    fix php cs

--- src/render.php
+++ src/render.php
@@ -41,7 +41,7 @@ if ( ! empty( $attributes['darkColorsPalette'] ) ) {
 	}
 }
 // Generate the CSS for the dark palette.
-if ( "light" === $theme_options ) {
+if ( 'light' === $theme_options ) {
 	$palette_styles = sprintf(
 		'html[data-theme="dark"] { %s %s prefers-color-scheme: dark;}',
 		$dark_colors,
@@ -94,15 +94,15 @@ wp_add_inline_script( 'mosne-dark-palette-inline', $inline_script_minified );
 ?>
 <li <?php echo wp_kses_data( get_block_wrapper_attributes( [ 'class' => $additional_classes ] ) ); ?>>
 	<div class="navigaiton-item__wrapper has-child"
-		 tabindex="-1"
-		 data-wp-interactive="mosne/dark-palette"
-		 data-wp-init="callbacks.colorInit"
-		 data-wp-on--mouseenter="actions.showSubmenu"
-		 data-wp-on--mouseleave="actions.hideSubmenu"
-		 data-wp-on--click="actions.showSubmenu"
-		 data-wp-on--keydown="actions.showSubmenu"
-		 data-wp-on--focusin="actions.showSubmenu"
-		 data-wp-on--focusout="actions.hideSubmenu"
+		tabindex="-1"
+		data-wp-interactive="mosne/dark-palette"
+		data-wp-init="callbacks.colorInit"
+		data-wp-on--mouseenter="actions.showSubmenu"
+		data-wp-on--mouseleave="actions.hideSubmenu"
+		data-wp-on--click="actions.showSubmenu"
+		data-wp-on--keydown="actions.showSubmenu"
+		data-wp-on--focusin="actions.showSubmenu"
+		data-wp-on--focusout="actions.hideSubmenu"
 		<?php
 		echo wp_kses_data(
 			wp_interactivity_data_wp_context(
